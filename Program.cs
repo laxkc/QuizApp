7e27ca9fcd54520806fb31a2fd603bb3597579a3
Laxman Kc <A00298022@mycambrian.ca>
2024-04-02 20:16:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+// Quiz application
+
+namespace QuizApplication
+{
+    public class QuizApplication()
+    {
+        static void Main(string[] args)
+        {
+            var quiz = new Dictionary<string, string>()
+            {
+                {
+                    "What is computer?", "one"
+                },
+                {
+                    "ASCII","Yes"
+                }
+            };
+
+            int score = 0;
+            int incorretAnswered = 0;
+            foreach (var item in quiz)
+            {
+                Console.Write($"{item.Key}: ");
+                var userChoice = Console.ReadLine();
+                if (userChoice == item.Value)
+                {
+                    score++;
+                    Console.WriteLine("Correct Answer!");
+                }
+                else
+                {
+                    incorretAnswered++;
+                    Console.WriteLine("Incorrect!");
+                }
+            }
+            Console.WriteLine($"Total Score: {score}");
+
+
+        }
+
+    }
+}
